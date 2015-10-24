88529d7235788277606f48ef27c47a1fa406269a
Liang Luo <llmainland@live.cn>
2015-10-24 13:21:52 -0700

    Checkpoint

--- Proposer.py
+++ Proposer.py
@@ -7,32 +7,49 @@ import socket
 import Queue
 
 class ServerNode(DatagramProtocol):
-    def __init__(self, acceptorCount, proposalNumberHop, startingRequestNumber):
+    def __init__(self, acceptorAddresses, proposalNumberHop, startingRequestNumber):
         self.ReadyList = [] #conceptually a list
-        self.AcceptorCount = acceptorCount
+        self.AcceptanceCount = 0
+        self.AcceptorCount = len(acceptorAddresses)
+        self.AcceptorAddresses = acceptorAddresses
         self.ProposalNumberHop = proposalNumberHop
         self.CurrentRequestNumber = startingRequestNumber
         self.CurrentInstanceId = 0
         self.RequestQueue = Queue.deque()
+        self.Helping = False
 
     def __TakeHop__():
-        self.CurrentInstanceId+=1
         self.CurrentRequestNumber+=self.ProposalNumberHop
         self.ReadyList = []
+        self.AcceptanceCount = 0
 
     def __PrintInternal__(content):
         print "[Proposer] " + content
+
+    def __IssuePromise__(value):
+        promiseObject = CommandObject(CommandType.Promise,
+                                      CurrentRequestNumber,
+                                      CurrentInstanceId,
+                                      value)
+        for address in self.AcceptorAddresses:
+            self.transport.write(CommandObject.ConvertToString(promiseObject),address)
 
     def datagramReceived(self, data, (host, port)):
         #what kind of command is it?
         cmdObj = CommandObject.ParseFromString(data)
         #this is an out of order message.
-        if cmdObj.RequestNumber != self.CurrentRequestNumber or cmdObj.InstanceId != self.CurrentInstanceId:
+        if cmdObj.Type != CommandType.Request and (cmdObj.RequestNumber != self.CurrentRequestNumber or cmdObj.InstanceId != self.CurrentInstanceId):
             return
         if(cmdObj.Type == CommandType.Request):
-            #fill later - application logic
+            #save it first, as we may be busy processing other things.
             self.RequestQueue.append(cmdObj.Value)
-            pass
+            if len(self.RequestQueue) == 1:
+                #this is the only pending request.  We can send this promise
+                #request out to start consensus.
+                #we do NOT allow in flight messages, that means the proposer
+                #can NOT get ANY message ahead.
+                #somewhat easy to change
+                __IssuePromise__(cmdObj.Value)
         elif cmdObj.Type == CommandType.Promise:
             #received a promise from a certain node.
             #do we have consensus for that instance?
@@ -49,10 +66,18 @@ class ServerNode(DatagramProtocol):
                     #before it starts consensus.
                     assert(len(self.RequestQueue) > 0)
                     #not safe to pop even if loss of message is not considered.
-                    #no guarentee of learners are made yet.
-                    acceptedValue = self.RequestQueue.popleft()
+                    #no guarentee of acceptors are determined yet.
+                    acceptedValue = self.RequestQueue[0]
+                    self.Helping = False
+
                 else:
                     acceptedValue = tuple([largestObj[3]])
+                    #The next series of Acceptance will result in a request to
+                    #NOT pop from the Queue.
+                    #We've learned that in this instance, someone has already
+                    #announced they have accepted some other value.  We must
+                    #push this value through (helping)
+                    self.Helping = True 
                 #broadcast this to this set.
                 acceptObj = CommandObject(CommandType.Accept,
                                           CurrentRequestNumber,
@@ -60,7 +85,8 @@ class ServerNode(DatagramProtocol):
                                           acceptedValue)
                 for address in [x[:2] in self.ReadyList]:
                     self.transport.write(CommandObject.ConvertToString(acceptObj),address)
-                __PrintInternal__("Consensus reached iid: %d, rid: %d, v: %s" % (CurrentInstanceId,CurrentRequestNumber,str(acceptedValue)))
+                #avoid other later messages to trigger this multiple times.
+                self.ReadyList = []
                 #no need to worry about this instance anymore, as recovery is
                 #not necessary.
                 #The burden is on acceptors - they could have promised to
@@ -70,13 +96,26 @@ class ServerNode(DatagramProtocol):
                 #the acceptors are free to accept new proposals anytime, and
                 #the learners can only commit after it receives majority vote
                 #for certain instance.
-                __TakeHop__()
         elif cmdObj.Type == CommandType.Denial:
-                #since the acceptance of accept messages are sent from acceptors, we require
+                #since the acceptance of accept messages are sent from
+                #acceptors, we require
                 #denial messages to be sent to proposer.
                 __TakeHop__()
         elif cmdObj.Type == CommandType.Acceptance:
                 #we need to accumulate acceptance per instance.
                 #if a majority of the acceptors have accepted a certain value,
                 #then that value has to be chosen.
-                pass
+                self.AcceptanceCount+=1
+                if self.AcceptanceCount > self.AcceptorCount / 2:
+                    #a consensus has been made.  fulfill this to
+                    #the state machine.
+                    __TakeHop__()
+                    self.CurrentInstanceId+=1
+                    #pop one of the pending messages if necessary.
+                    if self.Helping==False:
+                        self.RequestQueue.popleft()
+                    consensusValue = cmdObj.Value
+                    #deliver to state machine.
+                    if len(self.RequestQueue) != 0:
+                        #More work.
+                        __IssuePromise__(self.RequestQueue[0])
