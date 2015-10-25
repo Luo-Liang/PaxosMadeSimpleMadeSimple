9d20db9872bd34c9c2bd5d10dc3700cc259f0a7d
lmingcsce <Ming.Aaron.Liu@gmail.com>
2015-10-24 17:01:26 -0700

    sync

--- MainScript.py
+++ MainScript.py
@@ -4,12 +4,14 @@ from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 import argparse
 import sys
+import Acceptor
+import Proposer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--index',type=int, action='store')
 parser.add_argument('-s','--server-ips',nargs='+')
 parser.add_argument('-p','--server-ports',nargs='+')
-parseResult = parser.parse_args(sys.argv)
+parseResult = parser.parse_args(sys.argv[1:])
 servIndex = parseResult.index
 servIPs = parseResult.server_ips
 servPorts = [int(x) for x in parseResult.server_ports]
@@ -17,3 +19,6 @@ servCount = len(servPorts)
 thisPort = servPorts[servIndex]
 thisIP = servIPs[servIndex]
 
+reactor.listenUDP(thisPort, ServerNode())
+reactor.listenUDP(thisPort, AcceporNode())
+reactor.run()
